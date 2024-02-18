702021cd3b132e1acef6a7789b4f868411dae6ad
iqra9988 <itsiqrarehman@gmail.com>
2024-02-18 10:34:05 +0500

    done all five qls

--- index.js
+++ index.js
@@ -1,7 +1,85 @@
-let number = prompt(" plz enter a number:");
-let sum = 0;
+// js-classTask-00
 
-for (let digit of number) {
-  sum += parseInt(digit);
-}
-alert ("The sum of the digits  is: " + sum);
+// QUESTION = 1
+
+// Sum of all digits of a number.Like, 123 -> 1+2+3=6
+  // let num =  prompt("PlZ enter a number")
+  
+  // let Sum = 0;
+  // for(let i = 0; i < num.length; i++){
+    // Sum += parseInt(num[i]);
+//     console.log(num[i])
+//   }
+//  alert(Sum);
+
+
+
+// QUESTION = 2 
+
+// Sum of the range of 2 numbers. Like, num1=2, num2=5 -> 2+3+4+5=1
+
+// let firstNum =+prompt("Please enter the 1st number");
+//  let lastNum = +prompt("Please enter the 2nd number");
+//  let sum = 0;
+
+//  for (let i = firstNum; i <= lastNum; i++) {
+//    sum += i;
+//  }
+//  alert(sum);
+
+
+
+
+
+// QUESTION = 3
+
+// Calculate the average of numbers given in an array
+
+
+// const numbers = [ 10, 20,30,40,50];
+// let Sum = 0
+// for(let i = 0; i < numbers.length; i++){
+//     Sum += numbers[i];
+//     let average = Sum / numbers.length;
+//   }
+// console.log(average);
+
+
+
+// QUESTION = 4
+// Compare strings by count of characters. For example, car and bat are equal. 
+// car and care are not
+// let firstThing = prompt("enter a first thing")
+// let secondThing = prompt("enter a Second thing")
+// if(firstThing.length == secondThing.length){
+//   console.log("are equal")
+// }else{
+//   console.log("are not equal")
+// }
+// let word1 = "dog"
+// let word2 = "donkey"
+// let word3 = "cat"
+
+// if (word1.length ===word2.length){
+//   console.log(word1 + " and " + word2 + "are equal");
+// }
+
+
+// QUESTION = 5
+// swap the first and last character of the string
+
+// let word = prompt("enter your word")
+// if (word.length >=2){
+//   let firstCharacter = word[0];
+//   let lastCharacter = word[word.length-1];
+//   let swappedWord = lastCharacter + word.substring(1, word.length)
+//   console.log("original word:" , word);
+//   console.log("swapped Word:","swappedWord" );
+// }
+
+
+
+
+
+
+
